04a89fe10583df8c65ebe261f1597f7354e4f093
IR25721 <zunik0421@gmail.com>
2025-03-19 09:25:04 +0900

    agewasure

--- src/system/damage.rs
+++ src/system/damage.rs
@@ -1,11 +1,8 @@
 use rand;
 pub fn calculate_damage(total_attack: &f64, totoal_defense: &f64) -> f64 {
     let based_damage = (total_attack - 0.5 * totoal_defense) * 0.5;
-    println!("based_damage:{}", based_damage);
     if based_damage > 0. {
-        let damage = rand::random::<f64>() * (based_damage / 5. + 1.) + based_damage * 0.875;
-        println!("damage:{}", damage);
-        damage
+        rand::random::<f64>() * (based_damage / 5. + 1.) + based_damage * 0.875
     } else {
         0.
     }
